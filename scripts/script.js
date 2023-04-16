4e075eeba0dc08ed8058b25651d7cd1a49b40f84
Leticia Figueira <jlcfigueira@gmail.com>
2023-04-16 14:50:38 -0300

    Estrutura seção Sobre Nós, estiliza menu

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+// Torna o link de âncora mais suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
